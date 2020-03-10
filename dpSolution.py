03f03e128f2cb67d5f806ca3a9441e1d75d45b0b
Jack Weber <jackdavidweber@gmail.com>
2020-03-10 10:30:55 -0700

    works from commandLine

--- dpSolution.py
+++ dpSolution.py
@@ -42,7 +42,16 @@ def maxHeightOfSet(blocks, d):
     return (maxBlock, maxHeight)
 
 
-def maxHeight(l):
+def dpTableGenerator(l):
+    """
+    Generates a dp table that enables us to solve the problem
+    :param l:
+    :return: dp table
+        key: tuple that represents block that represents base of stack
+        value: dictionary with the following kv pairs
+            blockAbove: tuple of the block directly above base that results in largest height
+            height: height of the max stack with the given block as the base.
+    """
     dOfBases = {}
 
     for kv in l:
@@ -59,29 +68,57 @@ def maxHeight(l):
     #return {maxBase: dOfBases[maxBase]}
     return dOfBases
 
+def solution(d):
+    """
+    Takes dynamic table and returns solution in list form
+    :param d: dynamic table generated in dpTableGenerator
+    :return: list of tuples representing the maximum possible stack of blocks. First item is base of stack.
+    """
+    # find key with the maximum associated height
+    maxHeight = 0
+    maxBase = ()
+    for k in d.keys():
+        if (d[k]['height'] > maxHeight):
+            maxHeight = d[k]['height']
+            maxBase = k
+
+    # now construct list where each item represents a block. First item is the base
+    solutionList = [maxBase]
+    currBlock = maxBase
+
+    while len(d[currBlock]['blockAbove']) > 0:
+        solutionList.append(d[currBlock]['blockAbove'])
+        currBlock = d[currBlock]['blockAbove']
+
+    print("The tallest tower has " + str(len(solutionList)) + " blocks and a height of " + str(maxHeight))
+
+    return solutionList
+
+
+def tripToString(trip):
+    return str(trip[0]) + " " + str(trip[1]) + " " + str(trip[2]) + " "
+
 def main():
     """
     Can access program through commandline.
     First argument is the name of the inputfile
     Second argument is the name of the output file
     """
-    #infile = open(sys.argv[1], "r")
-    #outfile = open(sys.argv[2], "w")
+    infile = open(sys.argv[1], "r")
+    outfile = open(sys.argv[2], "w")
 
-    infile = open("/Users/jackweber/CS2020/CS140/BlockStacking/infile.txt", "r")
-    print(infile)
+    #infile = open("/Users/jackweber/CS2020/CS140/BlockStacking/infile.txt", "r")
+    #outfile = open("/Users/jackweber/CS2020/CS140/BlockStacking/outfile.txt", "w")
+
 
     inLines = infile.read().splitlines()
-    numBlockTypes = int(inLines.pop(0))
+    numBlockTypes = int(inLines.pop(0)) # FIXME: may not need to store this
 
     for i in range(0, len(inLines)):
         tl = inLines[i].split() # create a temporary list (tl) of the dimensions in the line
         inLines[i] = (int(tl[0]), int(tl[1]), int(tl[2]))
 
     blocks = allPossibleTuples(inLines)
-
-    #print(blocksOnTop((4, 4, 4),[(1, 10, 4)]))
-
 
     # now we want to create a list of key value pairs where keys are bases and values are list of blocks that can be on top of base
     lod = []
@@ -91,25 +128,14 @@ def main():
 
     # we now sort the list in increasing order of the number of potential blocks directly above base
     lod = sorted(lod, key=lambda i: len(i['blocksAbove']))
-    for kv in lod:
-        print(kv)
 
-    print(maxHeight(lod))
+    dpt = dpTableGenerator(lod)
+    solutionList = solution(dpt)
 
-    #sortedKeyList = sorted(d.keys(), key=)
+    outfile.write(str(len(solutionList)) + "\n")
+    for block in solutionList:
+        outfile.write(tripToString(block) + "\n")
 
-
-    """
-    # writes length of longest non-decreasing subsequence
-    outfile.write(str(1+tup[1]-tup[0]) + "\n")
-
-    # writes day on which subsequence begins
-    outfile.write(str(1+tup[0]) + "\n")
-
-    # writes lines giving the price of the stock on days in subsequence
-    for i in range(tup[0],tup[1]+1):
-        outfile.write(arr[i] + "\n")
-    """
 
 if __name__ == "__main__":
     main()
